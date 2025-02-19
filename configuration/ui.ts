f10eb9a22fae656e2ad03fe195b1bee4d04afb78
Emma Tillitski <emma.tillitski@gmail.com>
2025-02-19 14:22:10 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,8 +3,8 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Ask me anything!`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR CHAT`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, $Emma's AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with $Atlas, $Emma's AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
