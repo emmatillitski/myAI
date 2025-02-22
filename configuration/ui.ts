de4e50679e66a3817684063ced33a385a8f12481
Emma Tillitski <emma.tillitski@gmail.com>
2025-02-21 20:27:29 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Let's chat energy`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Ask me anything!`;
+export const FOOTER_MESSAGE: string = `Ask me about the energy industry! We can chat current events and trends, outlook for the future, or whatever else you may want to know!`;
 export const CLEAR_BUTTON_TEXT: string = `CLEAR CHAT`;
 export const PAGE_TITLE: string = `Chat with Joule, Emma's AI assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with Joule, Emma's AI assistant.`;
